ec92b3a4293db86e76c19359724f42311ffb6941
kiarakalra <71638118+kiarakalra@users.noreply.github.com>
2020-10-22 17:37:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristY=0;
 function setup() 
 {
     video=createCapture(VIDEO);
@@ -10,4 +15,26 @@ function setup()
 function modelLoaded() 
 {
     console.log("modelLoaded");
+}
+function draw() 
+{
+    background('#58e8dc');
+    document.getElementById("square_side").innerHTML="width and height of square will be="+difference+"px";
+    fill("yellow");
+    stroke("green");
+    square(noseX,noseY,difference);
+}
+function gotPoses(results) 
+{
+    if (results.length>0)
+     {
+       console.log(results); 
+       noseX=results[0].pose.nose.x;
+       noseY=results[0].pose.nose.y;
+       console.log("nosex="+noseX+"nosey="+noseY);
+       leftWristX=results[0].pose.leftWrist.x;
+       rightWristX=results[0].pose.rightWrist.x;
+       difference=floor(leftWristX-rightWristX);
+       console.log("leftWrist="+leftWristX+"rightWrist="+rightWristX); 
+    }
 }
